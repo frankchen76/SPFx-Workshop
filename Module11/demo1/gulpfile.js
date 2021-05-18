3ef2c6419059bffa2cd7569eabd9b1cf1dc54b78
Frank Chen <chentao76@hotmail.com>
2021-05-17 23:02:35 -0700

    upgrade to spfx 1.12.1 move the Azure CDN to FrankChenPFETest

--- Module11/demo1/gulpfile.js
+++ Module11/demo1/gulpfile.js
@@ -21,4 +21,13 @@ let subTaskRemove = build.subTask('remove-solution-subtask', function (gulp, bui
 });
 let taskRemove = build.task('remove-solution', subTaskRemove);
 
+var getTasks = build.rig.getTasks;
+build.rig.getTasks = function () {
+  var result = getTasks.call(build.rig);
+
+  result.set('serve', result.get('serve-deprecated'));
+
+  return result;
+};
+
 build.initialize(require('gulp'));
