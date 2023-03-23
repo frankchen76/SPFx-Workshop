0489953a01287da947bde33d5592d29fb8cc660c
Frank Chen <chentao76@hotmail.com>
2023-03-22 22:34:59 -0700

    upgrade to SPFx 1.16.1

--- Module11/src/webparts/module11Demo1/Module11Demo1WebPart.ts
+++ Module11/src/webparts/module11Demo1/Module11Demo1WebPart.ts
@@ -6,6 +6,7 @@ import {
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
 
 import * as strings from 'Module11Demo1WebPartStrings';
 import Module11Demo1 from './components/Module11Demo1';
@@ -15,7 +16,9 @@ export interface IModule11Demo1WebPartProps {
   description: string;
 }
 
-export default class Module11Demo1WebPart extends BaseClientSideWebPart <IModule11Demo1WebPartProps> {
+export default class Module11Demo1WebPart extends BaseClientSideWebPart<IModule11Demo1WebPartProps> {
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
 
   public render(): void {
     const element: React.ReactElement<IModule11Demo1Props> = React.createElement(
@@ -26,6 +29,55 @@ export default class Module11Demo1WebPart extends BaseClientSideWebPart <IModule
     );
 
     ReactDom.render(element, this.domElement);
+  }
+
+  protected onInit(): Promise<void> {
+    return this._getEnvironmentMessage().then(message => {
+      this._environmentMessage = message;
+    });
+  }
+  private _getEnvironmentMessage(): Promise<string> {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+        .then(context => {
+          let environmentMessage: string = '';
+          switch (context.app.host.name) {
+            case 'Office': // running in Office
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+              break;
+            case 'Outlook': // running in Outlook
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+              break;
+            case 'Teams': // running in Teams
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+              break;
+            default:
+              throw new Error('Unknown host');
+          }
+
+          return environmentMessage;
+        });
+    }
+
+    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+      this.domElement.style.setProperty('--link', semanticColors.link || null);
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+    }
+
   }
 
   protected onDispose(): void {
--- Module11/src/webparts/module11Demo1/loc/mystrings.d.ts
+++ Module11/src/webparts/module11Demo1/loc/mystrings.d.ts
@@ -2,6 +2,14 @@ declare interface IModule11Demo1WebPartStrings {
   PropertyPaneDescription: string;
   BasicGroupName: string;
   DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
 }
 
 declare module 'Module11Demo1WebPartStrings' {
